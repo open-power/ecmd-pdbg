1fc95adf9782c4d480fa0d2a650d737afa4e6baa
Lakshminarayana R. Kammath <lkammath@in.ibm.com>
2020-01-24 04:00:36 -0600

    instruction control enablement : start/stop all support

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,14 @@ CWD=`pwd`
 ARCH=`uname -m`
 echo "$ARCH"
 
-export EXT_CMD=""
+#set EXT_CMD and EXT_${EXT_CMD}_PATH required by makeext.py script to
+#auto generate ecmdExtInterpreter.C
+export EXT_CMD="cip"
+VAR1="EXT_"
+VAR2="${EXT_CMD}"
+VAR3="_PATH"
+EXT_PATH="$VAR1$VAR2$VAR3"
+export ${EXT_PATH}=${CWD}/ecmd/ecmd-core/ext/cip
 
 AUTOGEN_PATH="${CWD}/ecmd/src_"$ARCH
 export SRCPATH=$AUTOGEN_PATH
@@ -15,6 +22,7 @@ MAKE_EXT="${CWD}/ecmd/mkscripts/makeext.py"
 CREATE_ECMD_HELP="${CWD}/ecmd/ecmd-core/cmd/createEcmdHelp.pl"
 ECMD_EXT="${CWD}/ecmd/ecmd-core/capi"
 CIP_EXT="${CWD}/ecmd/ecmd-core/ext/cip/capi"
+TEMPLATE_EXT="${CWD}/ecmd/ecmd-core/ext/template/capi"
 
 declare -A comp
 comp=([ecmd]=${ECMD_EXT} [cip]=${CIP_EXT})
@@ -41,3 +49,35 @@ ecmdhelp_path="help/ecmd.htxt"
 echo "Generating ecmd.htxt"
 ${CREATE_ECMD_HELP} "${ecmdhelp_path}"
 
+#Generation of cip extension source files
+comp=([cip]=${TEMPLATE_EXT})
+
+declare -a templateFiles=(templateClientCapi.C templateDllCapi.C)
+
+#replace template with cip, Template with Cip and TEMPLATE with CIP
+#in template files
+EXTENSION_NAME_u=$(echo ${component} | tr 'a-z' 'A-Z')
+EXTENSION_NAME_u1=$(perl -e 'printf(ucfirst('${component}'))')
+
+#Using sed to replace the patterns in the template files with component name
+for infile in "${templateFiles[@]}"
+do
+    for component in "${!comp[@]}"
+    do
+        cd ${comp[${component}]}
+        cp -f ${infile} ${SRCPATH}
+        cd ${SRCPATH}
+        sed -i "s/template/${component}/g" ${infile}
+        sed -i "s/TEMPLATE/${EXTENSION_NAME_u}/g" ${infile}
+        sed -i "s/Template/${EXTENSION_NAME_u1}/g" ${infile}
+    done
+done
+
+declare -a cipExtFiles=(cipClientCapi.C cipDllCapi.C)
+
+#Renaming template with cip in file name
+for (( i=0; i<${#templateFiles[@]}; i++ ))
+do
+    echo "Generating ${cipExtFiles[i]}"
+    mv ${templateFiles[i]} ${cipExtFiles[i]}
+done
