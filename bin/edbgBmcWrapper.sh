2ad103d00b97e09e097548f60690ab25a5438620
Lakshminarayana R. Kammath <lkammath@in.ibm.com>
2020-01-28 10:26:15 -0600

    Picking right dtb file from /etc/pdata depending on platform
    Signed-off-by: Lakshminarayana R. Kammath <lkammath@in.ibm.com>

--- bin/edbgBmcWrapper.sh
+++ bin/edbgBmcWrapper.sh
@@ -4,8 +4,23 @@
 # help files will be searched under $EDBG_HOME/help/
 # ***************************************************************************
 export EDBG_HOME=/usr/
+PLAT_DTB_DIR=/etc/pdata
 
-export EDBG_DTB=/etc/pdata/rainier.dtb
+#Set default environment value to none
+EDBG_DTB=none
+
+for entry in "$PLAT_DTB_DIR"/*
+do
+  if [ -f "$entry" ];then
+     if [[ "$entry" == *".dtb"* ]]; then
+        EDBG_DTB=${entry}
+     else
+        echo "No DTB files found in the path"
+        exit 1
+     fi
+  fi
+done
+export EDBG_DTB
 
 # ***************************************************************************
 # Execute the edbg using relative path
